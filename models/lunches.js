4800f440d67634e03ef158a5babcac3b1c2539a8
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-05 21:13:00 -0800

    update input

--- models/lunches.js
+++ models/lunches.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const lunchSchema = new Schema({
-    lunchName: { type: String, required: true }
+    lunchName: { type: String, required: true },
+    restaurant: {type: String, required: true },
+    host: {type: String, required: true },
+    lunchType: {type: String, required: true }
 });
 
 const Lunches = mongoose.model("Lunches", lunchSchema);
