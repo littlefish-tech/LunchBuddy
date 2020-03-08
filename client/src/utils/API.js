1de95c640513af05fe8da2cc841ee568c022b5ee
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-07 23:06:23 -0800

    update util api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,27 @@
 import axios from "axios";
 
-export default {
+// export default {
     // getLunches: function() {
     //     return axios.get("/api/lunches");
     // },
 
-    saveLunch: function(lunch) {
-        console.log("lunch");
-        return axios.post("/api/lunches", lunch);
-    }
+//     saveLunch: function(lunch) {
+//         console.log("lunch");
+//         return axios.post("/api/lunches", lunch);
+//     }
+// };
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+export default {
+  getRestaurant: function() {
+    return axios.get("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + post.restaurant + "&location=94111");
+  },
+  getGroupDetail: function(id) {
+    return axios.get("/api/lunches" + id);
+  },
+
+  getGroupList: function() {
+    return axios.get("/api/lunches");
+  }
 };
