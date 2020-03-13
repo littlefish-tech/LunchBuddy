645a770bb8954ec70a8a59c4424e438435dd0496
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-12 22:27:43 -0700

    update files

--- client/src/components/Form/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Form(props) {
-    return <div>{props.children}</div>;
-  }
-  
-  export default Form;
-
--- client/src/components/FormGroup/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function FormGroup(props) {
-    return <div>{props.children}</div>;
-  }
-  
-  export default FormGroup;
