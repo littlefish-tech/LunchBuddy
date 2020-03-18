bf21182ea20d8d705bf401c30d5b7526c2596b7a
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-17 22:13:42 -0700

    update models

--- models/lunches.js
+++ models/lunches.js
@@ -5,7 +5,7 @@ const lunchSchema = new Schema({
     lunchName: { type: String, required: true },
     restaurant: {type: String, required: true },
     host: {type: String, required: true },
-    lunchType: {type: String, required: true },
+    lunchType: {type: String, required: false },
     lunchTime: {type: String, required: true },
     image: {type: String, required: false},
     restaurantLink: {type: String, required: true },
