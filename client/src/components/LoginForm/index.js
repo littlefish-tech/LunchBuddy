82a9e5b39872dc31d4063f2082e386e91782b867
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-18 17:24:42 -0700

    uopdate passpord

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -8,7 +8,7 @@ function LoginForm(props) {
     <div className="container" style={{ position: "absolute", top: "40vh", "text-align": "center" }}>
       <div className="signupform-group" style={{ width: "100vw" }} >
         <div><input className="login-form" onChange={props.handleLoginInput} name="username" placeholder="Username" value={props.value} /></div>
-        <div><input className="login-form" onChange={props.handleLoginInput} name="password" placeholder="Password" value={props.value} /></div>
+        <div><input className="login-form" onChange={props.handleLoginInput} name="password" type="password" placeholder="Password" value={props.value} /></div>
         <GroupButton onClick={props.handleLoginbtn}><Link to={props.redirectTo} style={{color: "black"}}>Login</Link></GroupButton>
   <div className="propMessage">{props.message}</div>
       </div>
